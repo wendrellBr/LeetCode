9e1cd59fc16e8c24c49e7f863f74416bbce39d7e
wendrellBr <wendrellpossatooliveira@gmail.com>
2024-08-31 07:57:15 -0300

    Resolvido exercício 35

---
+++ 35.search-insert-position.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=35 lang=python
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+ 
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
+# @lc code=end
+
