b96a5e7a97847482bd5dbbdce1e262e0be6e2b65
wendrellBr <wendrellpossatooliveira@gmail.com>
2024-09-09 23:14:54 -0300

    Resolvido codewars

--- codewars/Multiplesof3or5.js
+++ codewars/Multiplesof3or5.js
@@ -1,19 +1,14 @@
+// "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Additionally, if the number is negative, return 0.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
 function solution(number) {
   if (number <= 0) {
     return 0;
   }
 
-  function isDivisible(num, base) {
-    return num % base === 0;
-  }
-  let arr = []
-
-  for (let i = number; i > 0; i--) {
-      arr.map
-
-  }
-
-  let result = arr.reduce((acc, item) => acc * item, 1);
-
-  return result;
+  return [...Array(number).keys()]
+    .filter(i => i % 3 === 0 || i % 5 === 0)
+    .reduce((sum, i) => sum + i, 0);
 }
