a5a2074fc52b9dc6514a2fb9a891879a3a1b3614
wendrellBr <wendrellpossatooliveira@gmail.com>
2024-09-17 18:42:06 -0300

    Resolvido codewars

---
+++ codewars/CountOfPositivesSumOfNegatives.js
@@ -0,0 +1,19 @@
+function countPositivesSumNegatives(input) {
+  if (!input || input.length === 0) {
+    return [];
+  }
+
+  let countPositives = 0;
+  let sumNegatives = 0;
+
+  for (let item of input) {
+    if (item > 0) {
+      countPositives++;
+    } else if (item < 0) {
+      sumNegatives += item;
+    }
+  }
+
+  return [countPositives, sumNegatives];
+}
+  
