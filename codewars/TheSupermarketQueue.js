5bd35c916098cd1ebab25d32cfd3479b7f7a5545
Wendrell Possato Oliveira <90914846+wendrellBr@users.noreply.github.com>
2024-11-13 21:21:18 -0300

    .

---
+++ codewars/TheSupermarketQueue.js
@@ -0,0 +1,8 @@
+function queueTime(customers, n) {
+    var w = new Array(n).fill(0);
+    for (let t of customers) {
+      let idx = w.indexOf(Math.min(...w));
+      w[idx] += t;
+    }
+    return Math.max(...w);
+  }
